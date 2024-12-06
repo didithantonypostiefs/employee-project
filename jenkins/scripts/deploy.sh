6af2c47fcf7fb1a4b0e3b47bc7b6730a41a499a6
employeemanagement <employeemanagement@redslacker.com>
2024-12-06 17:00:31 +0530

    Added DB Part to Deploy script

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -14,4 +14,16 @@ cp -av /home/employeemanagement/DEPLOYMENT-BACKUPS/$(date +%F)/employee_manageme
 
 /home/employeemanagement/employee_managementenv/bin/python /home/employeemanagement/employee_management/manage.py collectstatic
 rsync -av /home/employeemanagement/employee_management/assets/admin /home/employeemanagement/employee_management/static/
+
+
+## Database Part
+mysqldump -h localhost -u root -p'redhat' --routines employee_management_db > /home/employeemanagement/DEPLOYMENT-BACKUPS/$(date +%F)/employee_management_db-${deploy_bkup_date}.sql
+if [[ "$?" -eq "0" ]];
+then
+        mysql -h localhost -u root -p'redhat' -e "DROP DATABASE employee_management_db;"
+        mysql -h localhost -u root -p'redhat' -e "CREATE DATABASE employee_management_db;"
+        mysql -h localhost -u root -p'redhat' employee_management_db < /home/employeemanagement/employee_management/data.sql
+fi
+touch /home/employeemanagement/employee_management/employee_management/wsgi.py
+
 set +x
